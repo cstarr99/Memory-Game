d3c97181e0da855bef96fbdbe39264a576b8ea30
Chase Starr <chasestarr9@gmail.com>
2025-07-14 22:41:16 -0400

    fixed some js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const submitBtn = document.querySelector(".submit");
+const submitBtn = document.querySelector("#reset-game");
+const shuffleBtn = document.querySelector("#shuffle-cards");
 const form = document.querySelector("#game-form");
 const gameUnit = Array.from(document.querySelectorAll(".game-unit"));
 
@@ -10,11 +11,31 @@ submitBtn.addEventListener("click", (e) => {
   });
 });
 
+shuffleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  gameUnit.forEach((unit) => {
+    unit.textContent = Math.floor(Math.random() * 9);
+    if (unit.textContent) {
+    }
+  });
+});
+
 document.addEventListener("click", (e) => {
   e.preventDefault();
   if (!e.target.matches(".game-unit")) return;
   e.target.classList.remove("hidden");
-  console.log("hell");
 });
 
 function turnCard(card) {}
+
+function randomizeCard(textContent) {}
+
+function addCheck() {
+  gameUnit.forEach((unit) => {
+    unit.filter((insideUnit) => {
+      console.log(insideUnit.textContent);
+    });
+  });
+}
+
+addCheck();
